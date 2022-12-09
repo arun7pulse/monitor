37c5366a8188f285eb66ffa10bde7bca9aa55e62
GMC-Arunsk.A <gmc-arunsk.a@GMC-ARUNSK-A-MAC.local>
2022-12-09 12:50:18 +0530

    updated the run.py

--- run.py
+++ run.py
@@ -3,22 +3,21 @@ import json
 import requests
 from pprint import pprint
 
-baseurl = "https://www.samsung.com/in/api"
-list_of_get = []
+from serviceability import call_serviceability
+from syndicate import call_syndicate
 
-with open("./url.csv", 'r') as file:
-  csvreader = csv.reader(file, delimiter=',')
-  for row in csvreader:
-    if(row[1]=='GET'):
-        list_of_get.append(f"{baseurl}{row[0]}")
+baseurl = "https://www.samsung.com/in/api" 
 
-print(list_of_get)
+if __name__ == '__main__':
+  responses =[]
 
-payload={}
-headers = {
-  'Cookie': 'ak_bmsc=414F4E9EFBB3B70F3499F2C2134FD1C0~000000000000000000000000000000~YAAQ3jpvPfhkbN+EAQAAF/pA5hIy/Zog43pFZHFIE+NTkwNgYRSA/LV6QGRWfnr8TYp+rF6U3yWrYLyk8FgkXviQxpihqFzIbOD4S8q4qRDsMckQ5L3wK1lWfG+hbYLgdZhbJWxbko8Bqj3O2XTcQ29nEpmj6GTrv67Ph1EuJl0urrxJaZRqUF4+OY/WsIPIre6qW1mlvsOSEVVTUmX/i8vyLM0Pet2hdKYSSnHGWTRZRKmEewW2teV9gIGL6pbdE3IeYHj1Xhi9YQAp4Os78yG2pDOI1UUI7vg+HqjQZxvlwXlZEna7EA0s4ZaMWHAYHrZlMYj7LUdqv55p97nw37MLsOTqcwjLTyFgEcZCi0fkXQM2BbsQyvqEOMLTJg=='
-}
+  response = call_serviceability() 
+  responses.append(response) 
+  # print(f"***{response.status_code}*** {response.url}")
 
-for url in list_of_get:
-    response = requests.request("GET", url, headers=headers, data=payload)
-    print(response.text)
+  response = call_syndicate()
+  responses.append(response)
+  [print(f"***{response.status_code}*** {response.url}") for response in responses]
+
+  
+
--- syndicate.py
+++ syndicate.py
@@ -1,12 +1,12 @@
 import requests
+from pprint import pprint 
 
-url = "https://www.samsung.com/in/api/v4/configurator/syndicated-product-linear?skus=SM-F936BZAD,SM-F936BZKD&component=offers,promotion,promotion_price,delivery_modes&count=0"
-
-payload={}
-headers = {
-  'Cookie': 'ak_bmsc=15AE2B7B3E773DE899AD7172905F9B32~000000000000000000000000000000~YAAQX2f2SMCgieKEAQAAEFlZ5xLUKVr6630OYEAC2xGL70v/v7ws8JCsYcxF0CWptH/syql2xJ03GTEuBs74oziPcZyi+BedjRzyqL9+iYLouALqIIKr6xMFtoZBgVMeCUENRcBa/QqU5rR11TV2mzfEHeSpauCfr2y0B5JC4BkY+VyTXBKwGluBRsNkhCJV9d9A9Twuur6NpclLTwsCS83glvkEbIYVNqOcWPEb+e/E/0QRyKWuApYr13b3+dXlg4YO69kNG3iEUYWcjabww6cjKgj5g3/FdA4MJLZCor8UNGRiOqSdYb5UOQJEeDoSLAXnzu3EviwjD7Yi2Qbk+/oX7zTqZiWjrlOxp3S9Iv2cL7xSZRHx2Aah1OBJmw==; bm_sv=D765DDF1F9F97B3DDBC977268447F6A5~YAAQX2f2SCA9i+KEAQAAPvxi5xLd4OwERrj89H/iTa/IUsfke9RBIlJ5g5XVamC5H93d/F51Rc8GpnPvQg7teoz8vUIK1rFQOf2SeTyGjIpLXKekyZz1J/fPevj+0w3AbRGkzg8Fx+tlZROMCGqwf/Xiu8r0AgHUWQEODZ8yQx0PhLMvYAODNmpPy7nrhuEKxo5V08doicSvQvwV+QB/VTWBhsJqk97jutY4kSlyHu10OqmntQs+TdXYpVc6qunDow==~1; country_codes=in; device_type=pc; country_region=CA-ON'
-}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
+def call_syndicate():
+  url = "https://www.samsung.com/in/api/v4/configurator/syndicated-product-linear?skus=SM-F936BZAD,SM-F936BZKD&component=offers,promotion,promotion_price,delivery_modes&count=0"
+  payload={}
+  headers = {}    #{'Cookie': 'ak_bmsc=414F4E9EFBB3B70F3499F2C2134FD1C0~000000000000000000000000000000~YAAQ3jpvPfhkbN+EAQAAF/pA5hIy/Zog43pFZHFIE+NTkwNgYRSA/LV6QGRWfnr8TYp+rF6U3yWrYLyk8FgkXviQxpihqFzIbOD4S8q4qRDsMckQ5L3wK1lWfG+hbYLgdZhbJWxbko8Bqj3O2XTcQ29nEpmj6GTrv67Ph1EuJl0urrxJaZRqUF4+OY/WsIPIre6qW1mlvsOSEVVTUmX/i8vyLM0Pet2hdKYSSnHGWTRZRKmEewW2teV9gIGL6pbdE3IeYHj1Xhi9YQAp4Os78yG2pDOI1UUI7vg+HqjQZxvlwXlZEna7EA0s4ZaMWHAYHrZlMYj7LUdqv55p97nw37MLsOTqcwjLTyFgEcZCi0fkXQM2BbsQyvqEOMLTJg=='}
+  response = requests.request("GET", url, headers=headers, data=payload)
+  return response
+if __name__ == '__main__':
+  response = call_syndicate()
+  print(f"***{response.status_code}*** {response.url}")
